2237d7a2e36b595be808d638438bb8b5092b2ed4
rahulsd380 <rahulsd380@gmail.com>
2025-08-04 09:50:11 +0600

    Fixed get profile issue

--- dist/app/modules/users/users.route.js
+++ dist/app/modules/users/users.route.js
@@ -8,27 +8,14 @@ exports.userRoutes = void 0;
 const express_1 = __importDefault(require("express"));
 const users_controller_1 = require("./users.controller");
 const auth_1 = __importDefault(require("../../middlewares/auth"));
-const user_constant_1 = require("./user.constant");
+const auth_constannts_1 = require("../auth/auth.constannts");
 // import { upload } from '../../utils/sendImageToCloudinary';
 const router = express_1.default.Router();
-router.get('/', (0, auth_1.default)(user_constant_1.UserRole.admin), users_controller_1.UserControllers.getAllUser);
-router.get('/me', (0, auth_1.default)('user', 'admin'), users_controller_1.UserControllers.getMe);
-router.get('/:userId', users_controller_1.UserControllers.getSingleUserById);
-router.get('/my-orders/:userId', (0, auth_1.default)('user', 'admin'), users_controller_1.UserControllers.getMyOrders);
-// router.get('/my-posts/:authorId', auth('user', 'admin'), UserControllers.getMyPosts);
-// router.put('/me', auth('user', 'admin'),
-//   multerUpload.single("file"),
-//   (req: Request, res: Response, next: NextFunction) => {
-//     if(req?.body?.data){
-//       req.body = JSON.parse(req?.body?.data);
-//     }
-//     next();
-//   },
+router.get("/", (0, auth_1.default)(auth_constannts_1.UserRole.admin), users_controller_1.UserControllers.getAllUser);
+router.get("/me", (0, auth_1.default)(auth_constannts_1.UserRole.user, auth_constannts_1.UserRole.admin, auth_constannts_1.UserRole.moderator, auth_constannts_1.UserRole["super-admin"]), users_controller_1.UserControllers.getMe);
+router.get("/:userId", users_controller_1.UserControllers.getSingleUserById);
 // UserControllers.updateProfile);
-router.delete('/remove-user/:userId', (0, auth_1.default)('admin'), users_controller_1.UserControllers.deleteUser);
-router.put('/make-admin/:userId', (0, auth_1.default)('admin'), users_controller_1.UserControllers.changeUserRoleToAdmin);
-router.put('/make-user/:userId', (0, auth_1.default)('admin'), users_controller_1.UserControllers.changeUserRoleToUser);
-router.put('/suspend-user/:userId', (0, auth_1.default)('admin'), users_controller_1.UserControllers.suspendUser);
-router.put('/follow/:userId', (0, auth_1.default)('user', 'admin'), users_controller_1.UserControllers.followUser);
-router.put('/unfollow/:userId', (0, auth_1.default)('user', 'admin'), users_controller_1.UserControllers.unfollowUser);
+router.delete("/remove-user/:userId", (0, auth_1.default)("admin"), users_controller_1.UserControllers.deleteUser);
+router.put("/make-admin/:userId", (0, auth_1.default)("admin"), users_controller_1.UserControllers.changeUserRoleToAdmin);
+router.put("/make-user/:userId", (0, auth_1.default)("admin"), users_controller_1.UserControllers.changeUserRoleToUser);
 exports.userRoutes = router;
--- src/app/modules/users/users.route.ts
+++ src/app/modules/users/users.route.ts
@@ -1,36 +1,40 @@
 // users.route.ts
-import express from 'express';
-import { UserControllers } from './users.controller';
-import auth from '../../middlewares/auth';
-import { UserRole } from './user.constant';
+import express from "express";
+import { UserControllers } from "./users.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "../auth/auth.constannts";
 // import { upload } from '../../utils/sendImageToCloudinary';
 
 const router = express.Router();
 
-router.get('/', auth(UserRole.admin), UserControllers.getAllUser);
-router.get('/me', auth('user', 'admin'), UserControllers.getMe);
-router.get('/:userId', UserControllers.getSingleUserById);
-router.get('/my-orders/:userId',auth('user', 'admin'), UserControllers.getMyOrders);
-// router.get('/my-posts/:authorId', auth('user', 'admin'), UserControllers.getMyPosts);
-
-// router.put('/me', auth('user', 'admin'),
-//   multerUpload.single("file"),
-//   (req: Request, res: Response, next: NextFunction) => {
-//     if(req?.body?.data){
-//       req.body = JSON.parse(req?.body?.data);
-//     }
-//     next();
-//   },
+router.get("/", auth(UserRole.admin), UserControllers.getAllUser);
+router.get(
+  "/me",
+  auth(
+    UserRole.user,
+    UserRole.admin,
+    UserRole.moderator,
+    UserRole["super-admin"]
+  ),
+  UserControllers.getMe
+);
+router.get("/:userId", UserControllers.getSingleUserById);
 // UserControllers.updateProfile);
 
-
-router.delete('/remove-user/:userId', auth('admin'), UserControllers.deleteUser);
-router.put('/make-admin/:userId', auth('admin'),  UserControllers.changeUserRoleToAdmin);
-router.put('/make-user/:userId', auth('admin'),  UserControllers.changeUserRoleToUser);
-router.put('/suspend-user/:userId', auth('admin'),  UserControllers.suspendUser);
-
-router.put('/follow/:userId', auth('user', 'admin'), UserControllers.followUser);
-router.put('/unfollow/:userId', auth('user', 'admin'), UserControllers.unfollowUser);
-
+router.delete(
+  "/remove-user/:userId",
+  auth("admin"),
+  UserControllers.deleteUser
+);
+router.put(
+  "/make-admin/:userId",
+  auth("admin"),
+  UserControllers.changeUserRoleToAdmin
+);
+router.put(
+  "/make-user/:userId",
+  auth("admin"),
+  UserControllers.changeUserRoleToUser
+);
 
 export const userRoutes = router;
